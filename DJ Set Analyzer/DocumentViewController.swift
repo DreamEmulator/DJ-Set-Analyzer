5a2cff0b78f2d43881d462221b25bee1811f7bd4
Sebastiaan Hols <s.hols@icloud.com>
2021-11-22 20:39:54 +0100

    Initial Commit

---
+++ DJ Set Analyzer/DocumentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DocumentViewController.swift
+//  DJ Set Analyzer
+//
+//  Created by Sebastiaan Hols on 22/11/2021.
+//
+
+import UIKit
+
+class DocumentViewController: UIViewController {
+    
+    @IBOutlet weak var documentNameLabel: UILabel!
+    
+    var document: UIDocument?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Access the document
+        document?.open(completionHandler: { (success) in
+            if success {
+                // Display the content of the document, e.g.:
+                self.documentNameLabel.text = self.document?.fileURL.lastPathComponent
+            } else {
+                // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+            }
+        })
+    }
+    
+    @IBAction func dismissDocumentViewController() {
+        dismiss(animated: true) {
+            self.document?.close(completionHandler: nil)
+        }
+    }
+}
